c6d5ccc19a01bf71740a26c3081273b1c0b2b523
Pete Gadomski <pete.gadomski@gmail.com>
2022-02-01 05:45:38 -0700

    feat: add File class
    This creates a common place for extracting version and product from
    MODIS file names.

---
+++ tests/test_file.py
@@ -0,0 +1,32 @@
+import pytest
+from stactools.modis.file import File
+
+from . import test_data
+
+
+def test_file() -> None:
+    path = test_data.get_path(
+        "data-files/MCD12Q1.A2001001.h00v08.006.2018142182903.hdf.xml")
+    file = File(path)
+    assert file.path == path
+    assert file.hdf_path == test_data.get_path(
+        "data-files/MCD12Q1.A2001001.h00v08.006.2018142182903.hdf")
+    assert file.xml_path == path
+    assert file.version == "006"
+    assert file.product == "MCD12Q1"
+    assert file.catalog_id == "MODIS/006/MCD12Q1"
+    assert file.id == "MCD12Q1.A2001001.h00v08.006.2018142182903"
+
+    path = test_data.get_path(
+        "data-files/MCD12Q1.A2001001.h00v08.006.2018142182903.hdf")
+    file = File(path)
+    assert file.xml_path == test_data.get_path(
+        "data-files/MCD12Q1.A2001001.h00v08.006.2018142182903.hdf.xml")
+    assert file.hdf_path == path
+
+    with pytest.raises(ValueError):
+        File("not cool")
+    with pytest.raises(ValueError):
+        File("metadata.xml")
+    with pytest.raises(ValueError):
+        File("not-enough-dots.hdf.xml")
