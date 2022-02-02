1fa25160989c2ccbef3cf4e699de82935e8fbd15
Pete Gadomski <pete.gadomski@gmail.com>
2022-02-02 08:43:31 -0700

    feat: add fragments
    Instead of using constants.py to hold metadata, we've moved all of the
    metadata to "fragments". These are JSON files that are included in the
    package via the "package data" mechanism. Each product currently has
    three fragments: `bands`, `collection`, and `item-properties`.
    
    This will enable easy addition of new products and their metadata
    without having to increase the size of the already-large constants.py
    file.

---
+++ scripts/create_fragments.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+"""Creates fragment files from constants.py.
+
+Should only need to be used for the initial migration from constants.py to
+fragments, but kept around for posterity. 
+"""
+
+import json
+import os
+from typing import List, cast
+
+from pystac import Link, Provider, Extent
+from pystac.extensions.eo import Band
+
+from stactools.modis.constants import MODIS_BAND_DATA, MODIS_CATALOG_ELEMENTS, ADDITIONAL_MODIS_PROPERTIES
+
+root = os.path.join(os.path.dirname(os.path.dirname(__file__)), "src",
+                    "stactools", "modis", "fragments")
+
+
+def make_fragments_directory(root: str, catalog_id: str) -> str:
+    parts = catalog_id.split("/")
+    path = os.path.join(root, parts[1], parts[2])
+    os.makedirs(path, exist_ok=True)
+    return path
+
+
+for catalog_id, data in MODIS_CATALOG_ELEMENTS.items():
+    directory = make_fragments_directory(root, catalog_id)
+    path = os.path.join(directory, "collection.json")
+    data["links"] = [
+        link.to_dict() for link in cast(List[Link], data["links"])
+    ]
+    data["providers"] = [cast(Provider, data["provider"]).to_dict()]
+    del data["provider"]
+    data["extent"] = cast(Extent, data["extent"]).to_dict()
+    with open(path, "w") as file:
+        json.dump(data, file, indent=4)
+
+for catalog_id, bands in MODIS_BAND_DATA.items():
+    directory = make_fragments_directory(root, catalog_id)
+    path = os.path.join(directory, "bands.json")
+    bands_as_dicts = [band.to_dict() for band in bands]
+    with open(path, "w") as file:
+        json.dump(bands_as_dicts, file, indent=4)
+
+for catalog_id, properties in ADDITIONAL_MODIS_PROPERTIES.items():
+    directory = make_fragments_directory(root, catalog_id)
+    path = os.path.join(directory, "item-properties.json")
+    with open(path, "w") as file:
+        json.dump(properties, file, indent=4)
