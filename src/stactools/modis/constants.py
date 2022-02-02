c1d5e705a12343b4eb6948de09bdaa5832def7ff
Pete Gadomski <pete.gadomski@gmail.com>
2022-02-02 14:05:16 -0700

    chore: fix quote type in constants.py

--- src/stactools/modis/constants.py
+++ src/stactools/modis/constants.py
@@ -1,3 +1,3 @@
-ITEM_COG_IMAGE_NAME = 'cog_image'
-ITEM_TIF_IMAGE_NAME = 'tif_image'
-ITEM_METADATA_NAME = 'metadata'
+ITEM_COG_IMAGE_NAME = "cog_image"
+ITEM_TIF_IMAGE_NAME = "tif_image"
+ITEM_METADATA_NAME = "metadata"
