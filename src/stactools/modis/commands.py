382216f5049c1a5a3f8478f54f232ef7456e715f
Pete Gadomski <pete.gadomski@gmail.com>
2022-02-02 13:22:03 -0700

    chore: renaming and cleanup of commands

--- src/stactools/modis/commands.py
+++ src/stactools/modis/commands.py
@@ -10,51 +10,49 @@ logger = logging.getLogger(__name__)
 
 
 def create_modis_command(cli: Group) -> Command:
-    """Creates a command group for commands working with
-    MODIS.
-    """
+    """Creates a command group for commands working with MODIS."""
 
-    @cli.group('modis', short_help=("Commands for working with "
-                                    "MODIS."))
+    @cli.group("modis", short_help=("Commands for working with MODIS."))
     def modis() -> None:
         pass
 
-    @modis.command('create-item',
-                   short_help='Create a STAC Item from a MODIS metadata file')
-    @click.argument('metadata_href')
-    @click.argument('dst')
-    @click.option('-c',
-                  '--cogify',
+    @modis.command("create-item",
+                   short_help="Create a STAC Item from a MODIS metadata file")
+    @click.argument("INFILE")
+    @click.argument("OUTDIR")
+    @click.option("-c",
+                  "--cogify",
                   is_flag=True,
-                  help='Convert the hdf into COG.')
-    def create_item_command(metadata_href: str, dst: str,
-                            cogify: bool) -> None:
-        """Creates a STAC Item based on metadata from an hdf.xml
-        MODIS file.
+                  help="Convert the hdf into COGs.",
+                  default=False)
+    def create_item_command(infile: str, outdir: str, cogify: bool) -> None:
+        """Creates a STAC Item based on metadata from an .hdf.xml MODIS file.
 
-        METADATA_REF is the modis metadata file. The
-        hdf file will be located in the same directory as that of METADATA_REF.
-        DST is directory that a STAC Item JSON file will be created
-        in. This will have a filename that matches the ID, which will
-        is the name of the METADATA_REF, without it's file extension.
+        Args:
+            infile (str): The source metadata xml file.
+            outdir (str): Directory that will contain the STAC Item.
+            cogify (bool, optional): Convert the .hdf file into multiple
+                Cloud-Optimized GeoTIFFs, one per layer. The .hdf file is
+                expected to reside alongside the metadata xml file.
         """
-
-        item = stac.create_item(metadata_href)
-
-        item_path = os.path.join(dst, '{}.json'.format(item.id))
-
+        item = stac.create_item(infile)
+        item_path = os.path.join(outdir, '{}.json'.format(item.id))
         item.set_self_href(item_path)
-
         if cogify:
             cog.create_cogs(item)
-
+        item.validate()
         item.save_object()
 
     @modis.command("cogify")
-    @click.argument("infile")
-    @click.argument("outdir")
+    @click.argument("INFILE")
+    @click.argument("OUTDIR")
     def cogify_command(infile: str, outdir: str) -> None:
-        """Converts a MODIS HDF file into one or more cloud optimized GeoTIFFs (COGs)."""
+        """Converts a MODIS HDF file into one or more cloud optimized GeoTIFFs (COGs).
+
+        Args:
+            infile (str): The source .hdf file. 
+            outdir (str): The directory that will contain the COGs.
+        """
         cog.cogify(infile, outdir)
 
     return modis
