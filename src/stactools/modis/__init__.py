7bef20dfaf924275be630853a1857288f029ce29
Pete Gadomski <pete.gadomski@gmail.com>
2022-02-03 09:37:40 -0700

    refactor!: remove `create_cogs` from __init__.py

--- src/stactools/modis/__init__.py
+++ src/stactools/modis/__init__.py
@@ -1,7 +1,6 @@
 import stactools.core
 from stactools.cli.registry import Registry
 
-from stactools.modis.cog import create_cogs
 from stactools.modis.stac import create_item
 
 stactools.core.use_fsspec()
@@ -15,4 +14,4 @@ def register_plugin(registry: Registry) -> None:
 __version__ = "0.1.0"
 """Library version"""
 
-__all__ = ["create_item", "create_cogs"]
+__all__ = ["create_item"]
