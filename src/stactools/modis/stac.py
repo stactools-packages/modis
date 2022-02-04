ed3388f683090a5106426e4c61ce6c83f955c290
Pete Gadomski <pete.gadomski@gmail.com>
2022-02-04 07:26:36 -0700

    feat: use a metadata class to extract information
    There's some dangling questions here, including what to do about
    multiple platforms, and what to do about datetimes.

--- src/stactools/modis/stac.py
+++ src/stactools/modis/stac.py
@@ -1,15 +1,12 @@
-import xml.etree.ElementTree as ET
-
 import pystac
 from pystac import Collection, Item, MediaType
 from pystac.extensions.eo import Band, EOExtension
 from pystac.extensions.item_assets import AssetDefinition, ItemAssetsExtension
-from pystac.utils import str_to_datetime
-from shapely.geometry import shape
 
 import stactools.modis.fragment
 from stactools.modis.constants import HDF_ASSET, METADATA_ASSET
 from stactools.modis.file import File
+from stactools.modis.metadata import Metadata
 
 
 def create_collection(product: str, version: str) -> Collection:
@@ -59,76 +56,24 @@ def create_item(infile: str) -> Item:
         pystac.Item: A STAC Item representing this MODIS image.
     """
     file = File(infile)
-    metadata_root = ET.parse(file.xml_path).getroot()
-
-    # Item id
-    product_element = metadata_root.find(
-        "GranuleURMetaData/CollectionMetaData/ShortName")
-    assert product_element is not None
-    product = product_element.text
-    version_element = metadata_root.find(
-        "GranuleURMetaData/CollectionMetaData/VersionID")
-    assert version_element is not None
-    version = version_element.text
-    if version == "6":
-        version = "006"
-    elif version == "61":
-        version = "061"
-    else:
-        raise ValueError(f"Unsupported MODIS version: {version}")
-
-    image_name = metadata_root.find(
-        "GranuleURMetaData/DataFiles/DataFileContainer/DistributedFileName")
-    assert image_name is not None
-    assert image_name.text is not None
-    id = image_name.text.replace(".hdf", "")
-
-    coordinates = []
-    point_ele = "{}/{}".format(
-        "GranuleURMetaData/SpatialDomainContainer/",
-        "HorizontalSpatialDomainContainer/GPolygon/Boundary/Point")
-    for point in metadata_root.findall(point_ele):
-        lon = point.find("PointLongitude")
-        assert lon is not None
-        assert lon.text is not None
-        lat = point.find("PointLatitude")
-        assert lat is not None
-        assert lat.text is not None
-        coordinates.append([float(lon.text), float(lat.text)])
-
-    geom = {"type": "Polygon", "coordinates": [coordinates]}
-
-    bounds = shape(geom).bounds
-
-    # Item date
-    prod_node = "GranuleURMetaData/ECSDataGranule/ProductionDateTime"
-    prod_dt_text = metadata_root.find(prod_node)
-    assert prod_dt_text is not None
-    assert prod_dt_text.text is not None
-    prod_dt = str_to_datetime(prod_dt_text.text)
-
+    metadata = Metadata(file.xml_path)
     item = pystac.Item(
-        id=id,
-        geometry=geom,
-        bbox=bounds,
-        datetime=prod_dt,
+        id=metadata.id,
+        geometry=metadata.geometry,
+        bbox=metadata.bbox,
+        datetime=metadata.datetime,
         properties=stactools.modis.fragment.load_item_properties(
-            product, version))
+            metadata.product, metadata.version))
 
     # Common metadata
-    collection = stactools.modis.fragment.load_collection(product, version)
+    # TODO should we remove these? They're in the collection
+    collection = stactools.modis.fragment.load_collection(
+        metadata.product, metadata.version)
     item.common_metadata.providers = collection["providers"]
     item.common_metadata.description = collection["description"]
 
-    instrument_short_name = metadata_root.find(
-        "GranuleURMetaData/Platform/Instrument/InstrumentShortName")
-    assert instrument_short_name is not None
-    assert instrument_short_name.text is not None
-    item.common_metadata.instruments = [instrument_short_name.text]
-    platform_short_name = metadata_root.find(
-        "GranuleURMetaData/Platform/PlatformShortName")
-    assert platform_short_name is not None
-    item.common_metadata.platform = platform_short_name.text
+    item.common_metadata.instruments = metadata.instruments
+    item.common_metadata.platform = metadata.platform
     item.common_metadata.title = collection["title"]
 
     # Hdf
@@ -150,8 +95,8 @@ def create_item(infile: str) -> Item:
     # Bands
     eo = EOExtension.ext(item.assets[HDF_ASSET], add_if_missing=True)
     eo.bands = [
-        Band(band)
-        for band in stactools.modis.fragment.load_bands(product, version)
+        Band(band) for band in stactools.modis.fragment.load_bands(
+            metadata.product, metadata.version)
     ]
 
     return item
