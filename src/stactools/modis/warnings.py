11f4ddf6b82f64c4ee8a47f039fb10b7ce1bf45c
Pete Gadomski <pete.gadomski@gmail.com>
2022-02-15 08:10:07 -0700

    feat: warn when skipping proj extension

---
+++ src/stactools/modis/warnings.py
@@ -0,0 +1,12 @@
+from pystac import Item
+
+from stactools.modis.file import File
+
+
+class MissingProj(UserWarning):
+    """Raised when there is missing proj information on an item."""
+
+    def __init__(self, item: Item, file: File):
+        """Creates a new missing proj warning from an item and a file."""
+        super().__init__(f"No PROJ information on {item.id}, "
+                         f"{file.hdf_href} is a remote file or not present")
