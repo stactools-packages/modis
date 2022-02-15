680c899258a4e6575c0791bc9134aae80fc5daa5
Pete Gadomski <pete.gadomski@gmail.com>
2022-02-15 08:23:27 -0700

    feat: add cog assets

--- scripts/create_expected.py
+++ scripts/create_expected.py
@@ -11,6 +11,9 @@ data_files_directory = os.path.join(os.path.dirname(os.path.dirname(__file__)),
                                     "tests", "data-files")
 expected_directory = os.path.join(data_files_directory, "expected")
 
+cog_product = "MCD12Q1"
+cog_version = "006"
+
 for file_name in os.listdir(data_files_directory):
     if os.path.splitext(file_name)[1] == ".xml":
         file = File(os.path.join(data_files_directory, file_name))
@@ -20,8 +23,12 @@ for file_name in os.listdir(data_files_directory):
                                        file.version, "collection.json")
         collection.set_self_href(collection_path)
 
-        item_file_name = f"{file.id}.json"
-        item = stactools.modis.stac.create_item(file.href)
+        if file.product == cog_product and file.version == cog_version:
+            cog_directory = data_files_directory
+        else:
+            cog_directory = None
+        item = stactools.modis.stac.create_item(file.href,
+                                                cog_directory=cog_directory)
         item.validate()
 
         collection.add_item(item)
