debe697e965310f5ee02c83006f05d126893a782
Pete Gadomski <pete.gadomski@gmail.com>
2022-02-15 09:09:55 -0700

    feat: add raster bands to cogs
    Still need to test this

--- src/stactools/modis/warnings.py
+++ src/stactools/modis/warnings.py
@@ -10,3 +10,12 @@ class MissingProj(UserWarning):
         """Creates a new missing proj warning from an item and a file."""
         super().__init__(f"No PROJ information on {item.id}, "
                          f"{file.hdf_href} is a remote file or not present")
+
+
+class MissingRasterBand(UserWarning):
+    """Raised when there is missing raster band information."""
+
+    def __init__(self, item: Item, subdataset_name: str):
+        """Creates a new missing raster band inforamtion."""
+        super().__init__(
+            f"Missing raster band for {item.id}: {subdataset_name}")
