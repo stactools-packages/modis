f251dc28cb8fbbe9354e2c28e40e878f9422b23a
Pete Gadomski <pete.gadomski@gmail.com>
2022-03-03 07:03:05 -0700

    fix: cog subdataset names
    Includes skipped tests that are used to check for correctness. They're skipped
    because they're very long and their data is heavy.

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,3 +1,75 @@
 from stactools.testing.test_data import TestData
 
-test_data = TestData(__file__)
+test_data = TestData(
+    __file__, {
+        "MOD13A1.A2022033.h09v05.061.2022051005006.hdf": {
+            "url":
+            "https://ai4epublictestdata.blob.core.windows.net/stactools/modis"
+            "/MOD13A1.A2022033.h09v05.061.2022051005006.hdf"
+        },
+        "MOD13A1.A2022033.h09v05.061.2022051005006.hdf.xml": {
+            "url":
+            "https://ai4epublictestdata.blob.core.windows.net/stactools/modis"
+            "/MOD13A1.A2022033.h09v05.061.2022051005006.hdf.xml"
+        },
+        "MOD13Q1.A2022033.h09v05.061.2022051005101.hdf": {
+            "url":
+            "https://ai4epublictestdata.blob.core.windows.net/stactools/modis"
+            "/MOD13Q1.A2022033.h09v05.061.2022051005101.hdf"
+        },
+        "MOD13Q1.A2022033.h09v05.061.2022051005101.hdf.xml": {
+            "url":
+            "https://ai4epublictestdata.blob.core.windows.net/stactools/modis"
+            "/MOD13Q1.A2022033.h09v05.061.2022051005101.hdf.xml"
+        },
+        "MOD14A1.A2022049.h09v05.061.2022057235503.hdf": {
+            "url":
+            "https://ai4epublictestdata.blob.core.windows.net/stactools/modis"
+            "/MOD14A1.A2022049.h09v05.061.2022057235503.hdf"
+        },
+        "MOD14A1.A2022049.h09v05.061.2022057235503.hdf.xml": {
+            "url":
+            "https://ai4epublictestdata.blob.core.windows.net/stactools/modis"
+            "/MOD14A1.A2022049.h09v05.061.2022057235503.hdf.xml"
+        },
+        "MYD10A1.A2022059.h09v05.061.2022061043435.hdf": {
+            "url":
+            "https://ai4epublictestdata.blob.core.windows.net/stactools/modis"
+            "/MYD10A1.A2022059.h09v05.061.2022061043435.hdf"
+        },
+        "MYD10A1.A2022059.h09v05.061.2022061043435.hdf.xml": {
+            "url":
+            "https://ai4epublictestdata.blob.core.windows.net/stactools/modis"
+            "/MYD10A1.A2022059.h09v05.061.2022061043435.hdf.xml"
+        },
+        "MYD13A1.A2022041.h09v05.061.2022059190534.hdf": {
+            "url":
+            "https://ai4epublictestdata.blob.core.windows.net/stactools/modis"
+            "/MYD13A1.A2022041.h09v05.061.2022059190534.hdf"
+        },
+        "MYD13A1.A2022041.h09v05.061.2022059190534.hdf.xml": {
+            "url":
+            "https://ai4epublictestdata.blob.core.windows.net/stactools/modis"
+            "/MYD13A1.A2022041.h09v05.061.2022059190534.hdf.xml"
+        },
+        "MYD13Q1.A2022041.h09v05.061.2022059190716.hdf": {
+            "url":
+            "https://ai4epublictestdata.blob.core.windows.net/stactools/modis"
+            "/MYD13Q1.A2022041.h09v05.061.2022059190716.hdf"
+        },
+        "MYD13Q1.A2022041.h09v05.061.2022059190716.hdf.xml": {
+            "url":
+            "https://ai4epublictestdata.blob.core.windows.net/stactools/modis"
+            "/MYD13Q1.A2022041.h09v05.061.2022059190716.hdf.xml"
+        },
+        "MYD14A1.A2022049.h09v05.061.2022057234556.hdf": {
+            "url":
+            "https://ai4epublictestdata.blob.core.windows.net/stactools/modis"
+            "/MYD14A1.A2022049.h09v05.061.2022057234556.hdf"
+        },
+        "MYD14A1.A2022049.h09v05.061.2022057234556.hdf.xml": {
+            "url":
+            "https://ai4epublictestdata.blob.core.windows.net/stactools/modis"
+            "/MYD14A1.A2022049.h09v05.061.2022057234556.hdf.xml"
+        },
+    })
--- tests/test_cog.py
+++ tests/test_cog.py
@@ -10,6 +10,15 @@ import stactools.modis.stac
 from tests import test_data
 
 SUBDATASET_NAMES = ["Eight_Day_Snow_Cover", "Maximum_Snow_Extent"]
+FAILING_COG_FILE_NAMES = [
+    "MOD14A1.A2022049.h09v05.061.2022057235503.hdf",
+    "MOD13Q1.A2022033.h09v05.061.2022051005101.hdf",
+    "MOD14A1.A2022049.h09v05.061.2022057235503.hdf",
+    "MYD10A1.A2022059.h09v05.061.2022061043435.hdf",
+    "MYD13A1.A2022041.h09v05.061.2022059190534.hdf",
+    "MYD13Q1.A2022041.h09v05.061.2022059190716.hdf",
+    "MYD14A1.A2022049.h09v05.061.2022057234556.hdf",
+]
 
 
 class CogTest(TestCase):
@@ -49,3 +58,18 @@ class CogTest(TestCase):
             for subdataset_name in SUBDATASET_NAMES
         ]
         self.assertEqual(set(file_names), set(expected_cog_names))
+
+
+@pytest.mark.parametrize("file_name", FAILING_COG_FILE_NAMES)
+@pytest.mark.skip(reason="these are very long and were used for a one-time fix"
+                  )
+def test_failing_cogs(file_name: str) -> None:
+    """These tests are from an initial attempt at cogifying all 36 modis V061
+    products in late February 2022.
+    """
+    infile = test_data.get_external_data(file_name)
+    _ = test_data.get_external_data(f"{file_name}.xml")
+    item = stactools.modis.stac.create_item(infile)
+    with TemporaryDirectory() as temporary_directory:
+        _, _ = stactools.modis.cog.cogify(infile, temporary_directory)
+        stactools.modis.cog.add_cogs(item, temporary_directory, create=False)
