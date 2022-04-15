55a0bb0ea164895625a36cad6e30c05fdda4d94b
Pete Gadomski <pete.gadomski@gmail.com>
2022-04-15 13:02:26 -0600

    release: v0.3.0a0
    We're doing an alpha release so we can take up any changes as we kick the tyres
    on the COG support.

--- src/stactools/modis/__init__.py
+++ src/stactools/modis/__init__.py
@@ -13,7 +13,7 @@ def register_plugin(registry: Registry) -> None:
     registry.register_subcommand(commands.create_modis_command)
 
 
-__version__ = "0.2.0"
+__version__ = "0.3.0a0"
 """Library version"""
 
 __all__ = ["create_item", "cogify"]
